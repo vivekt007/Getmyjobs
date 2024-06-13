d81d38cfcf8f3a333d6ac8ada879e0b6b5786f75
vivekt007 <vivektigadi59@gmail.com>
2024-06-13 16:54:55 +0530

    "Updated HTML, CSS, and SVG files, removed an SVG image, and modified JavaScript functions."

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "20%";
 }
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
